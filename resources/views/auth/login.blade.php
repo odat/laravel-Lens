c9702d3b47f4dd1db7182d743443b0b4a1d3e322
Moath Odat <moatho@myhsm.net>
2025-04-17 14:20:43 +0100

    Update login form action URL to use application URL for improved consistency

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <div class="card shadow-lg border-0 rounded-lg mt-5">
             <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
             <div class="card-body">
-                <form action="{{route('laravel-lens-auth.auth')}}" method="post">
+                <form action="{{ config('app.url') }}{{route('laravel-lens-auth.auth', [], false) }}" method="post">
                     @csrf
                     <div class="form-floating mb-3">
                         <input class="form-control" id="inputEmail" type="text" placeholder="Username" name="username" />
